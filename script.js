ca0147f787a6b69610abe4b65a9ba6e2c8b3b17d
mulegi <eugenemulegi6@gmail.com>
2025-06-10 06:04:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('newGridBtn');
     const gridContainer = document.getElementById('grid-container');
-    const gridSize = 16;
-    const totalCells = gridSize * gridSize;
+    const CONTAINER_SIZE = 1000
 
-    for (let i = 0; i < totalCells; i++) {
-        const gridCell = document.createElement('div');
-        gridCell.classList.add('grid-cell');
-        gridContainer.appendChild(gridCell);
+    function createNewGrid(squarePerSide) {
+    gridContainer.innerHTML = '';
+
+    const squareSize = CONTAINER_SIZE / squarePerSide;
+
+    let totalSquares = squarePerSide * squarePerSide;
+
+    for (i = 0; i < totalSquares; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        gridSquare.style.width = `${squareSize}px`;
+        gridSquare.style.height = `${squareSize}px`;
+
+        gridSquare.addEventListener('mouseenter', () => {
+            gridSquare.classList.add('hovered');
+        });
+        gridContainer.appendChild(gridSquare);
     }
-})
+    }
+
+    createNewGrid(16);
+
+    button.addEventListener('click', () => {
+    let userInput = prompt('Enter the number of squares per side for the new grid (max 100):');
+    let squarePerSide = parseInt(userInput);
+
+    if (isNaN(squarePerSide) || squarePerSide <= 0 || squarePerSide > 100) {
+        alert('Please enter a valid number between 1 and 100.');
+    } else {
+        createNewGrid(squarePerSide);
+    }
+    });
+});
